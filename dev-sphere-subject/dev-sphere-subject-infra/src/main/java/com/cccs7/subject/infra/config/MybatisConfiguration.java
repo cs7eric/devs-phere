accd41b79a1114d50be73258ba5f56d4a2f8a6b6
cs7eric <csq020611@gmail.com>
2025-02-19 22:21:01 +0800

    feat: sql 拦截器自动翻译

---
+++ dev-sphere-subject/dev-sphere-subject-infra/src/main/java/com/cccs7/subject/infra/config/MybatisConfiguration.java
@@ -0,0 +1,22 @@
+package com.cccs7.subject.infra.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis配置
+ *
+ * @author cccs7 - csq020611@gmail.com
+ * @date 2025/02/19
+ */
+@Configuration
+public class MybatisConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new MybatisPlusAllSqlLog());
+        return mybatisPlusInterceptor;
+    }
+}
