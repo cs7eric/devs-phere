a946e3e5e39502948b082b0cdd308c3ffb6b7c77
cccs7 <csq020611@gmail.com>
2025-03-01 19:30:21 +0800

    feat/gateway: redis拉取权限信息

--- dev-sphere-gateway/src/main/java/com/cccs7/gateway/auth/SaTokenConfigure.java
+++ dev-sphere-gateway/src/main/java/com/cccs7/gateway/auth/SaTokenConfigure.java
@@ -4,7 +4,6 @@ import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.reactor.filter.SaReactorFilter;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
-import cn.dev33.satoken.util.SaResult;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -34,12 +33,6 @@ public class SaTokenConfigure {
                 SaRouter.match("/oss/**", r -> StpUtil.checkLogin());
                 SaRouter.match("/subject/subject/add", r -> StpUtil.checkPermission("subject:add"));
                 SaRouter.match("/subject/**", r -> StpUtil.checkLogin());
-
-            })
-            // 异常处理方法：每次setAuth函数出现异常时进入 
-            .setError(e -> {
-                return SaResult.error(e.getMessage());
-            })
-            ;
+            });
     }
 }
--- dev-sphere-gateway/src/main/java/com/cccs7/gateway/auth/StpInterfaceImpl.java
+++ dev-sphere-gateway/src/main/java/com/cccs7/gateway/auth/StpInterfaceImpl.java
@@ -1,9 +1,13 @@
 package com.cccs7.gateway.auth;
 
 import cn.dev33.satoken.stp.StpInterface;
+import com.cccs7.gateway.redis.RedisUtil;
+import com.google.gson.Gson;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.LinkedList;
+import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,24 +19,53 @@ import java.util.List;
 @Component
 public class StpInterfaceImpl implements StpInterface {
 
+    @Resource
+    private RedisUtil redisUtil;
+
+    private final String AUTH_PERMISSION_PREFIX = "auth.permission";
+    private final String AUTH_ROLE_PREFIX = "auth.role";
+
+
+    /**
+     * 获取权限列表
+     *
+     * @param loginId   登录ID
+     * @param loginType 登录类型
+     * @return {@link List }<{@link String }>
+     */
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
-        // 返回此 loginId 拥有的权限列表
-        // 1. 数据库
-        // 2. redis
-        // 3. redis 缓存，没有的话，去调用微服务查
-        List<String> permissionList = new LinkedList<>();
-        permissionList.add("user:add");
 
-        return permissionList;
+        return getAuthList(loginId.toString(), AUTH_PERMISSION_PREFIX);
+
     }
 
+    /**
+     * 获取角色列表
+     *
+     * @param loginId   登录ID
+     * @param loginType 登录类型
+     * @return {@link List }<{@link String }>
+     */
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
-        List<String> roleList = new LinkedList<>();
-        roleList.add("admin");
+        return getAuthList(loginId.toString(), AUTH_ROLE_PREFIX);
+    }
 
-        return roleList;
+
+    /**
+     * 获取授权列表
+     *
+     * @param loginId 登录ID
+     * @param prefix  前缀
+     * @return {@link List }<{@link String }>
+     */
+    private List<String> getAuthList(String loginId, String prefix) {
+        String authKey = redisUtil.buildKey(prefix, loginId.toString());
+        String authValue = redisUtil.get(authKey);
+        if (StringUtils.isBlank(authValue)) return Collections.emptyList();
+
+        return new Gson().fromJson(authValue, List.class);
     }
 
 }
