a4a88cbd21c972d9115e60b0162c1121efb2f7f2
cccs7 <csq020611@gmail.com>
2025-03-27 21:01:58 +0800

    feat/nacos-wx: wx 服务注册到 nacos

--- dev-sphere-subject/dev-sphere-subject-application/dev-sphere-subject-application-controller/src/main/java/com/cccs7/subject/application/interceptor/LoginInterceptor.java
+++ dev-sphere-subject/dev-sphere-subject-application/dev-sphere-subject-application-controller/src/main/java/com/cccs7/subject/application/interceptor/LoginInterceptor.java
@@ -21,8 +21,6 @@ public class LoginInterceptor implements HandlerInterceptor {
         return true;
     }
 
-
-
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         LoginContextHolder.remove();
